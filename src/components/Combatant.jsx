e1f9fc0eeeb556bc95f6904982d72b16794c0800
Taylor Musolf <tcmusolf@gmail.com>
2024-07-25 14:49:59 -0700

    working on layering data between combatant and encounter

--- src/components/Combatant.jsx
+++ src/components/Combatant.jsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
-function Combatant({name, isCurrent, isPC}){
-
+const Combatant = ({combatantName, isCurrent, isPC, maxHP, currentHP, combatants, setCombatants}) => {
     const [hp, setHp] = useState(100);
+    const [maxHp, setMaxHp] = useState(maxHP);
+    const [characterName, setCharacterName] = useState(combatantName);
+    const [notes, setNotes] = useState('');
 
+    function handleHpIncrease(){
+        if(hp + 1 <= maxHp){
+            setHp(hp + 1)
+        }
+    }
+    function handleHpDecrease(){
+        if(hp - 1 >= 0){
+            setHp(hp - 1)
+        }
+    }
     return (
         <>
             <div>
-                {isCurrent && <strong>{'--> '}</strong>}
-                <span>{name}</span>
-                <span> {hp} HP</span>
-                <span onClick={()=> setHp(hp + 1)}>+</span>
-                <span onClick={()=> setHp(hp - 1)}>-</span>
+                {/* <span>{combatantName}</span> */}
+                <span><input type="text" value={characterName} onChange={(e) => setCharacterName(e.currentTarget.value)} /></span>
+                {/* <span> {hp} HP</span>
+                <span onClick={handleHpIncrease}>+ </span>
+                <span onClick={handleHpDecrease}>- </span>
+                <span>Notes:<input type="textarea" value={notes} onChange={(e)=> setNotes(e.currentTarget.value)} /></span> */}
             </div>
         </>
     )
--- src/components/Encounter.jsx
+++ src/components/Encounter.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import React, { useMemo, useState,} from "react";
 import Combatant from "./Combatant";
 function Encounter(){
-
     const [round, setRound] = useState(1); //encounter round
     const [currentIdx, setCurrentIdx] = useState(0); //current initiative in round
-    const [combatants, setCombatants] = useState(["Taylor", "Kyle", "Karisa", "Jane", "Josh"]); //array of pcs and npcs
+    const combatants = [{name: "Taylor", maxHP: 100, isPC: true, currentHP: 100}, {name: "Karisa", maxHP: 50, isPC: true, currentHP: 50}, {name: "Bugbear", maxHP: 200, isPC: false, currentHP: 50}];
+    const memoizedCombatants = combatants.map((combatant, i) => <Combatant key={i} combatantName={combatant.name}/>);
+
+    const [stateCombatants, setStateCombatants] = useState(memoizedCombatants);
     const [delayedCombatants, setDelayedCombatants] = useState([]);
 
 
     function handleNext(){
         let newIdx = currentIdx + 1;
-        if(newIdx >= combatants.length){
+        if(newIdx >= stateCombatants.length){
             newIdx = 0;
             setRound(round + 1);
         }
@@ -20,22 +22,52 @@ function Encounter(){
     function handlePrevious(){
         let newIdx = currentIdx - 1;
         if(newIdx < 0){
-            newIdx = combatants.length - 1;
+            newIdx = stateCombatants.length - 1;
             setRound(round - 1);
         }
         setCurrentIdx(newIdx);
     }
+    function handleDelay(idx){
+        if(idx === stateCombatants.length - 1){
+            setRound(round + 1);
+            setCurrentIdx(0);
+        }
+        setDelayedCombatants((prev) =>[...prev, stateCombatants[idx]]);
+        setStateCombatants((prev) => prev.slice(0, idx).concat(prev.slice(idx + 1)));
+    }
+    function handleUndelay(idx){
+        setStateCombatants((prev) => prev.slice(0, currentIdx).concat(delayedCombatants[idx], prev.slice(currentIdx)));
+        setDelayedCombatants((prev) => prev.slice(0, idx).concat(prev.slice(idx + 1))); //remove from delayed
+    }
+    function handleUp(idx){
+        setStateCombatants((prev) => prev.slice(0, idx - 1).concat(stateCombatants[idx], stateCombatants[idx - 1], prev.slice(idx + 1)));
+    }
+    function handleDown(idx){
+        setStateCombatants((prev) => prev.slice(0, idx).concat(stateCombatants[idx + 1], stateCombatants[idx], prev.slice(idx + 2)));
+    }
 
     return(
         <>
+            {delayedCombatants.map((delayedC, i)=> {
+                return(
+                    <div key={i}>
+                        {delayedC}
+                        <button onClick={() => handleUndelay(i)}>Act Now?</button>
+                    </div>
+                )
+            })}
             <h1>Round {round}</h1>
-            {combatants.map((combatant, i) => {
+            {stateCombatants.map((combatant, i) => {
                 const isCurrent = currentIdx === i
-        
-                //arrow pointing up
-                //arrow pointing down
-                //delay button
-                return <Combatant key={i} isCurrent={isCurrent} name={combatant} isPC={false}/>
+                return(
+                    <div key={i}>
+                        {isCurrent && <strong>{'--> '}</strong>}
+                        {combatant}
+                        <button onClick={() => handleUp(i)} disabled={i === 0}>Up</button>
+                        <button onClick={() => handleDown(i)} disabled={i === stateCombatants.length - 1}>Down</button>
+                        {isCurrent && <button onClick={() => handleDelay(i)}>Delay?</button>}
+                    </div>
+                ) 
             })}
             <button onClick={handlePrevious} disabled={round === 1 && currentIdx === 0} >Previous</button>
             <button onClick={handleNext}>Next</button>
